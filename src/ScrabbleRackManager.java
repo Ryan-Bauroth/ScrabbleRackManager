6913174ff2ba2cb454a9bec34af45ebffda48527
Ryan Bauroth <25bauroth@da.org>
2023-03-22 12:27:47 -0400

    set up auto draw system

--- src/ScrabbleRackManager.java
+++ src/ScrabbleRackManager.java
@@ -1,4 +1,6 @@
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Scrabble Rack Manager
@@ -8,9 +10,31 @@ import java.util.ArrayList;
 public class ScrabbleRackManager {
     private ArrayList<String> database;
     private final String alpha = "abcdefghijklmnopqrstuvwxyz";
+    private final int[] occurrences = {9, 2, 2, 4, 12, 2, 3, 2, 9, 1, 1, 4, 2, 6, 8, 2, 1, 6, 4, 6, 4, 2, 2, 1};
+    private ArrayList<String> bag;
+    private ArrayList<String> myTiles;
 
     public ScrabbleRackManager(){
         database = new ArrayList<>();
+        //adds the correct numbers of letters to the bag
+        for(int i = 0; i < 100; i++){
+            for(int x = 0; x < occurrences[i]; x++){
+                bag.add(alpha.substring(i, i+1));
+            }
+        }
+        //draws from the bag
+        for(int i = 0; i < 7; i++){
+            myTiles.add(bag.get((int)(Math.random() * bag.size())));
+        }
+        try{
+            Scanner in = new Scanner(new File("new_scrabble.txt"));
+            while(in.hasNext()){
+                database.add(in.nextLine());
+            }
+        }
+        catch(Exception e){
+
+        }
     }
 
     /** displays the contents of the player's tile rack */
